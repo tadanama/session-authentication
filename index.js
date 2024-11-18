3272c1e97d743847e7eeedcbfc8f9e08949800a6
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-18 11:54:04 +0800

    Set and configure express-session as middleware

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import session from "express-session";
+import env from "dotenv";
+
+env.config();
 
 const app = express();
 const port = 3000;
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		saveUninitialized: false,
+		resave: false,
+		name: "sesiTemuduga",
+		cookie: {
+			maxAge: 1000 * 60 * 60,
+		},
+	})
+);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
